ca48ba5514edfa2cfd4a9a402c7b58a1e939860c
lizhe <lizhe19851001@126.com>
2016-12-23 18:41:15 +0800

    m

---
+++ ch7/http4.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	db := database{"shoes": 50, "socks": 5}
+	http.HandleFunc("/list", db.list)
+	http.HandleFunc("/price", db.price)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
