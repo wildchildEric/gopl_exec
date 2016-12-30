0e93320fbc8c05c4c2d1d02538b368c6d593a7c9
lizhe <lizhe19851001@126.com>
2016-12-30 18:17:44 +0800

    m

---
+++ ch8/du2.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"flag"
+	"time"
+)
+
+var verbose = flag.Bool("v", false, "show verbose progress messages")
+
+func main() {
+	// ...start background goroutine...
+	// Print the results periodically.
+	var tick <-chan time.Time
+	if *verbose {
+		tick = time.Tick(500 * time.Millisecond)
+	}
+	var nfiles, nbytes int64
+loop:
+	for {
+		select {
+		case size, ok := <-fileSizes:
+			if !ok {
+				break loop // fileSizes was closed
+			}
+			nfiles++
+			nbytes += size
+		case <-tick:
+			printDiskUsage(nfiles, nbytes)
+		}
+	}
+	printDiskUsage(nfiles, nbytes) // final totals
+}
